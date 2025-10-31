fc3fd422e1c291228dfbe59b0a5f11e8c49bee99
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2025-10-31 09:58:29 -0300

    feat: adicionando a estrutura de autenticação e o retorno do token

--- src/app/useCases/get-user-by-email-useCase.ts
+++ src/app/useCases/get-user-by-email-useCase.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
+
 import { getUserJson } from "../../utils/get-user-json";
-export function getUserByEmailUseCase(email: string): boolean {
+export function getUserByEmailUseCase(email: string): any {
   const users = getUserJson();
   return users.find((user: { email: string }) => user.email === email);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { createUserUseCase } from "./app/useCases/create-user-useCase";
+import { signUp } from "./app/useCases/sign-up";
 
-const newUser = createUserUseCase({
+const user  = {
   name: "John Doe",
   email: "john.doe@example.com",
-  password: "securepassword123",
-});
-console.log("New user created:", newUser);
+  password: "securepassword123"
+}
+
+const sign = signUp({
+  email: user.email,
+  password: user.password
+})
+console.log("Sign up successful:", sign); 
+
